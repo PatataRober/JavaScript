42b54021477e87d8224d815420344362a3f52f66
Roberto.Gomez <roberto.gomez.fpi@gmail.com>
2025-01-17 10:22:39 +0100

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,23 @@
+// Definimos 4 variables para el perfil de un usuario
+let nombre = "Rober";
+let edad = 19;
+let correo = "roberto.gomez.fpi@gmail.com";
+let pais = "España";
+
+// Imprimimos las variables iniciales
+console.log("Perfil inicial:");
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Correo:", correo);
+console.log("País:", pais);
+console.log("\n");
+
+// Cambiamos una de las variables (por ejemplo, la edad)
+edad = "20";
+
+// Imprimimos las variables después de los cambios
+console.log("Perfil actualizado:");
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Correo:", correo);
+console.log("País:", pais);
