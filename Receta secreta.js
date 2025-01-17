dd7e27da6765a49d2e95decd47eb01f5d12812e8
Roberto.Gomez <roberto.gomez.fpi@gmail.com>
2025-01-17 10:16:49 +0100

    Add files via upload

---
+++ Receta secreta.js
@@ -0,0 +1,48 @@
+// ·Título 
+console.log("RECETA PARA LA MEJOR TORTILLA DE PATATA");
+console.log();
+// ·Segúndo titulo
+console.log("INGREDIENTES");
+console.log("");
+/* ·Ingredientes 
+   ·Página de referencia: https://www.directoalpaladar.com/recetas-de-huevos-y-tortillas/empezando-en-la-cocina-receta-de-tortilla-de-patatas-con-cebolla
+*/
+
+console.log("·Patatas ............... 700g");
+console.log("·Cebollas .............. 300g");
+console.log("·Huevos ................... 6");
+console.log("·Sal .............. Una pizca");
+console.log("·Aceite de oliva ... Bastante");
+console.log();
+
+// ·Tiempo de la receta
+console.log("Tiempo total ........... 45 m");
+console.log("Elaboración ............ 30 m");
+console.log("Cocción ................ 15 m");
+console.log();
+
+// ·Receta
+console.log("Comenzamos con la tarea más larga, la de caramelizar la cebolla que nos llevará unos 30 minutos.")
+console.log("Para ello, pelamos la cebolla y la cortamos en juliana.");
+console.log("Después la ponemos en una sartén a fuego muy lento y dejamos que se haga muy despacio, removiendo de vez en cuando.");
+console.log("No nos interesa que se dore la cebolla sino que se vaya pochando muy despacio.");
+console.log("");
+console.log("Mientras la cebolla se hace, pelamos las patatas y las cortamos en rodajas finas, procurando que todas ellas sean de tamaño uniforme.");
+console.log("Las dejamos en agua durante 15 minutos y ponemos una sartén con aceite de oliva abundante en el fuego.");
+console.log("");
+console.log("Sin dar tiempo a que el aceite se caliente, añadimos las patatas y dejamos que se vayan friendo muy despacio, partiendo de un aceite casi en frío.");
+console.log("Así conseguimos que las patatas se confiten en lugar de dorarse.");
+console.log("De todas formas, cuando lleven unos diez minutos y hayamos removido de vez en cuando, podemos subir el fuego para conseguir que algunas de las patatas queden más tostaditas, originando así contrastes en el plato final.");
+console.log("");
+console.log("Sacamos las patatas y las escurrimos bien del aceite y las ponemos en un bol grande.");
+console.log("Escurrimos la cebolla cuando esté en su punto, y la ponemos sobre las patatas.");
+console.log("Batimos los huevos y los añadimos al bol, removiendo con un tenedor para que se mezclen bien los tres ingredientes.");
+console.log("Cuajamos la tortilla en una sartén con una cucharada de aceite durante unos tres o cuatro minutos y le damos la vuelta.");
+console.log("Para ayudar a los que no sean muy duchos en esa operación, existen en el mercado sartenes dobles que permiten dar la vuelta a la tortilla sin riesgo de que se nos derrame.");
+console.log("");
+
+// ·Ingrediente secreto
+let secreto = "Como ingrediente secreto vamos a retirar un ingrediente en vez de añadirlo, si le quitas la cebolla esta mucho mejor que con ella.";
+secreto = "++++++++++[>+++++++>+++++++++++>+++++++++++>+++++++++++>+++>+++++++++++>+++++++++++>++++++++++>+++++++++++>++++++++++>++++++++++>+++++++++++>++++++++++>+++++++++++>++++++++++++>++++++++++>+++>++++++++++++>++++++++++>++++++++++>+++++++++++>++++++++++>++++++++++++>+++++++++++>+++>++++++++++++>++++++++++>+++++++++++>+++++++++++>++++++++++++>+++>++++++++++>+++>+++++++++++>++++++++++>++++++++++++>+++++++++++>+++++++++++>++++++++++>+++++++++++>+++>++++++++++++>+++++++++++>+++>+++++++++++>+++++++++++>++++++++++>+++++++++++>++++++++++>++++++++++>+++++++++++>++++++++++>+++++++++++>++++++++++++>++++++++++>+++>++++++++++>+++++++++++>+++>++++++++++++>++++++++++>++++++++++++>+++>++++++++++>++++++++++>+++>++++++++++>++++++++++++++++++++>++++++++++++++++++>++++++++++>++++++++++>+++++++++++>+++++++++++>+++++++++++>+++++++++++>++++>+++>++++++++++++>+++++++++++>+++>+++++++++++>++++++++++>+++>+++++++++++>++++++++++++>+++++++++++>++++++++++++>++++++++++>++++++++++++>+++>+++++++++++>++++++++++>+++>++++++++++>++++++++++>++++++++++>+++++++++++>+++++++++++>++++++++++>+++>++++++++++>++++++++++++>++++++++++++>++++++++++>+++>+++++++++++>++++++++++++>++++++++++>++++++++++>+++++++++++>+++>+++++++++++>++++++++++>+++++++++++>+++++++++++>+++++++++++>+++>+++++++++++>++++++++++++>++++++++++>+++>++++++++++>+++++++++++>+++++++++++>+++>++++++++++>+++++++++++>++++++++++>+++++<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<-]>---.>+.>-.>+.>++.>-----.>.>+++.>++++.>+.>.>-----.>+.>.>----.>+.>++.>-----.>+.>-.>++++.>+.>----.>+.>++.>--.>---.>-.>+.>-----.>++.>---.>++.>++++.>+.>----.>-----.>++++.>---.>++++.>++.>---.>.>++.>-----.>.>+++.>++++.>+.>.>-----.>+.>.>----.>+.>++.>+.>.>++.>--.>+.>++.>++.>.>+.>++.>---.>-----.>---.>---.>.>-----.>++++.>--.>+.>++++.>++.>-----.>-----.>++.>--.>+.>++.>+++.>---.>-----.>----.>---.>-----.>++.>--.>---.>++.>-.>+.>--.>+.>--..>---.>++.>+.>-----.>----.>---.>++.>-.>---.>-.>++++.>+.>++.>-.>+.>----.>+.>++++.>++.>+++.>---.>+.>++.>-.>+.>.>++.>+.>--..>---.>----.";
+console.log(secreto);
+console.log("");
